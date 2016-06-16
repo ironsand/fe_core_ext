5468a56260cc79ec1b6e561c25be97a5421f3124
Tetsu <eisensand@gmail.com>
2016-06-16 18:46:05 +0700

    decimal

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in fe_core_ext.gemspec
 gemspec
+
+gem 'bigdecimal'
--- spec/fe_core_ext_spec.rb
+++ spec/fe_core_ext_spec.rb
@@ -4,8 +4,4 @@ describe FeCoreExt do
   it 'has a version number' do
     expect(FeCoreExt::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
