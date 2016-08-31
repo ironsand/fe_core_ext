e3c55a8a3cd37f8038f7289d6e066ec6edce1648
Tetsu <eisensand@gmail.com>
2016-08-31 16:59:53 +0700

    sum と mean を追加

--- lib/fe_core_ext/core_ext/array.rb
+++ lib/fe_core_ext/core_ext/array.rb
@@ -1,3 +1,6 @@
+require 'bigdecimal'
+require 'bigdecimal/util'
+
 module FeCoreExt::CoreExt
 end
 
@@ -7,6 +10,14 @@ module FeCoreExt::CoreExt::Array
     sorted = sort
     len = sorted.length
     (sorted[(len - 1) / 2] + sorted[len / 2]) / 2.0
+  end
+
+  def sum
+    inject(0) { |result, el| result + el }
+  end
+
+  def mean
+    sum.to_d / size
   end
 end
 
