fad76fa81bde8fd6675219a0d529ba7d8549f0f2
Tetsu <eisensand@gmail.com>
2018-08-31 06:27:31 +0700

    add Array#remove, #remove_at, #remove_if

--- lib/fe_core_ext/core_ext/array.rb
+++ lib/fe_core_ext/core_ext/array.rb
@@ -16,6 +16,33 @@ module FeCoreExt::CoreExt::Array
     return if empty?
     sum.to_d / size
   end
+
+  # remove, remove_at, remove_if はこちらのものを使わせてもらっています。
+  # https://gist.github.com/nekoTheShadow/2de8a324f445bdde8a67
+  def remove(val)
+    # ブロックが渡されており、かつvalと等しい要素が見つからなかった場合
+    return yield if block_given? && !self.include?(val)
+
+    temp = self.dup
+    temp.delete(val)
+    temp
+  end
+
+  def remove_at(pos)
+    temp = self.dup
+    temp.delete_at(pos)
+    temp
+  end
+
+  def remove_if
+    temp = self.dup
+    # ブロックを持たない場合
+    return temp.delete_if unless block_given?
+
+    self.each{|val| temp.delete(val) if yield(val)}
+    temp
+  end
+
 end
 
 class Array
