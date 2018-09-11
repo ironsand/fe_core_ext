ea0ee9789172b6fd6005126a58db5e941e57e461
Tetsu <eisensand@gmail.com>
2018-09-11 15:13:05 +0700

    add Time#floor

---
+++ lib/fe_core_ext/core_ext/time.rb
@@ -0,0 +1,24 @@
+require 'active_support'
+require 'active_support/time'
+
+module FeCoreExt::CoreExt
+end
+
+module FeCoreExt::CoreExt::Time
+  def floor(unit = :sec)
+    case unit
+    when :sec
+      Time.zone.local(year, month, day, hour, min, sec)
+    else
+      self
+    end
+  end
+end
+
+module FeCoreExt::CoreExt::TimeClassMethods
+end
+
+class Time
+  include FeCoreExt::CoreExt::Time
+  extend FeCoreExt::CoreExt::TimeClassMethods
+end
