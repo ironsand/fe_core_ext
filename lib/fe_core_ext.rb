be2d86c8bdfefd5561fef68b1e3f9083c1f1eb83
Tetsu <eisensand@gmail.com>
2018-09-11 22:48:32 +0700

    fix_require_bug

--- lib/fe_core_ext.rb
+++ lib/fe_core_ext.rb
@@ -1,5 +1,14 @@
 require 'fe_core_ext/version'
-Dir.glob("#{Dir.pwd}/lib/fe_core_ext/core_ext/*.rb").each{|file| require file }
+require 'fe_core_ext/core_ext/array'
+require 'fe_core_ext/core_ext/csv'
+require 'fe_core_ext/core_ext/date'
+require 'fe_core_ext/core_ext/hash'
+require 'fe_core_ext/core_ext/integer'
+require 'fe_core_ext/core_ext/numeric'
+require 'fe_core_ext/core_ext/pathname'
+require 'fe_core_ext/core_ext/string'
+require 'fe_core_ext/core_ext/time'
+require 'fe_core_ext/core_ext/uri'
 
 module FeCoreExt
 end
