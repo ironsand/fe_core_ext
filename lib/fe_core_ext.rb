cf8edbf8f2cbd190ad8e7534e3f02d69fd65d73f
Tetsu <eisensand@gmail.com>
2018-09-11 15:26:21 +0700

    requireを整理

--- lib/fe_core_ext.rb
+++ lib/fe_core_ext.rb
@@ -1,15 +1,5 @@
 require 'fe_core_ext/version'
-# require 'fe_core_ext/core_ext/array'
-# require 'fe_core_ext/core_ext/string'
-# require 'fe_core_ext/core_ext/hash'
-# require 'fe_core_ext/core_ext/csv'
-# require 'fe_core_ext/core_ext/pathname'
-# require 'fe_core_ext/core_ext/date'
-# require 'fe_core_ext/core_ext/integer'
-# require 'fe_core_ext/core_ext/numeric'
-# require 'fe_core_ext/core_ext/uri'
-require 'fe_core_ext/core_ext/time'
-Dir.glob('fe_core_ext/core_ext/*.rb').each{|file| require file }
+Dir.glob("#{Dir.pwd}/lib/fe_core_ext/core_ext/*.rb").each{|file| require file }
 
 module FeCoreExt
 end
