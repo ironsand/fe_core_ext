60beabe0565d73cb9ac95602068d04ca76e7912e
ironsand <eisensand@gmail.com>
2023-11-09 17:51:10 +0900

    Ferrumのwait_for に複数選択を追加

--- lib/fe_core_ext/gem_ext/ferrum.rb
+++ lib/fe_core_ext/gem_ext/ferrum.rb
@@ -2,15 +2,23 @@
 
 module Ferrum
   class Browser
-    delegate :wait_for_css, :wait_for_xpath, to: :page
+    delegate %i[wait_for_css wait_for_csses wait_for_xpath wait_for_xpaths], to: :page
   end
 
   class Page
-    delegate :wait_for_css, :wait_for_xpath, to: :main_frame
+    delegate %i[wait_for_css wait_for_csses wait_for_xpath wait_for_xpaths], to: :main_frame
   end
 
   class Frame
     module DOM
+      def wait_for_csses(selector, timeout: 3)
+        wait_for_selector(selector, :css, timeout)
+      end
+
+      def wait_for_xpaths(selector, timeout: 3)
+        wait_for_selector(selector, :xpath, timeout)
+      end
+
       def wait_for_css(selector, timeout: 3)
         wait_for_selector(selector, :at_css, timeout)
       end
@@ -24,8 +32,8 @@ module Ferrum
       def wait_for_selector(selector, selector_method, timeout)
         interval = 0.1
         (timeout / interval).to_i.times do
-          node = send(selector_method, selector)
-          return node if node
+          nodes = send(selector_method, selector)
+          return nodes if nodes
           sleep(interval)
         end
         nil
