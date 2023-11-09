9267c3e130dd116b20cac3d063f4ec1a812d23a2
ironsand <eisensand@gmail.com>
2023-11-09 18:03:04 +0900

    fix

--- lib/fe_core_ext/gem_ext/ferrum.rb
+++ lib/fe_core_ext/gem_ext/ferrum.rb
@@ -2,11 +2,11 @@
 
 module Ferrum
   class Browser
-    delegate %i[wait_for_css wait_for_csses wait_for_xpath wait_for_xpaths], to: :page
+    delegate :wait_for_css, :wait_for_csses, :wait_for_xpath, :wait_for_xpaths, to: :page
   end
 
   class Page
-    delegate %i[wait_for_css wait_for_csses wait_for_xpath wait_for_xpaths], to: :main_frame
+    delegate :wait_for_css, :wait_for_csses, :wait_for_xpath, :wait_for_xpaths, to: :main_frame
   end
 
   class Frame
