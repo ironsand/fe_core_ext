bd6b0c1c37ce70865d4f6c1ae005adae2e0c0d87
ironsand <eisensand@gmail.com>
2025-02-23 17:08:11 +0900

    fix bugs

--- lib/fe_core_ext.rb
+++ lib/fe_core_ext.rb
@@ -1,6 +1,5 @@
 require_relative "fe_core_ext/version"
 require_relative "fe_core_ext/core_ext/array"
-require_relative "fe_core_ext/core_ext/csv"
 require_relative "fe_core_ext/core_ext/date"
 require_relative "fe_core_ext/core_ext/hash"
 require_relative "fe_core_ext/core_ext/integer"
